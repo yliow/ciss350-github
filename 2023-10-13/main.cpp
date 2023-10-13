9e8ca46f3b0ccfd361519782c4f0b9614648d2c6
Yihsiang Liow <yliow@ccis.edu>
2023-10-13 13:26:00 -0400

    no msg

--- 2023-10-13/main.cpp
+++ 2023-10-13/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <exception>
 
 // file: SLNode.h (SL = singly-linked)
 class SLNode
@@ -24,19 +25,48 @@ std::ostream & operator<<(std::ostream & cout, const SLNode & n)
 
 
 // file: SLList.h and SLList.cpp
+
+class Underflow: public std::exception
+{};
+
 class SLList
 {
 public:
     SLList()
         : phead_(NULL)
     {}
+    ~SLList()
+    {
+        std::cout << "in SLList::~SLList() ...\n";
+        while (!is_empty())
+        {
+            delete_head();
+        }
+    }
     void insert_head(int key)
     {
         phead_ = new SLNode(key, phead_);
     }
+    bool is_empty() const
+    {
+        return (phead_ == NULL);
+    }
+    void delete_head()
+    {
+        if (is_empty())
+        {
+            throw Underflow();
+        }
+        else
+        {   
+            SLNode * p = phead_;
+            phead_ = phead_->next_;
+            delete p;
+        }
+    }
     int head() const
     {
-        return (*phead_).key_;
+        return phead_->key_;
     }
     SLNode * phead_;
 };
@@ -85,25 +115,44 @@ int main()
 
     // print_list(p5);
 
-    SLList list;
-    std::cout << list << '\n';
+    int x = 0;
 
-    std::cout << "inserting head with 1 ...\n";
-    list.insert_head(1);
-    std::cout << list << '\n';
 
-    std::cout << "inserting head with 0 ...\n";
-    list.insert_head(0);
-    std::cout << list << '\n';
+    if (x == 0)
+    {
+        SLList list;
+        std::cout << list << '\n';
+        
+        std::cout << "inserting head with 1 ...\n";
+        list.insert_head(1);
+        std::cout << list << '\n';
+        
+        std::cout << "inserting head with 0 ...\n";
+        list.insert_head(0);
+        std::cout << list << '\n';
+        
+        std::cout << "inserting head with 3 ...\n";
+        list.insert_head(3);
+        std::cout << list << '\n';
+        
+        std::cout << "inserting head with 5 ...\n";
+        list.insert_head(5);
+        std::cout << list << '\n';
 
-    std::cout << "inserting head with 3 ...\n";
-    list.insert_head(3);
-    std::cout << list << '\n';
+        // test delete when empty
+        try
+        {
+            SLList l1;
+            l1.delete_head();
+        }
+        catch (Underflow & e)
+        {
+            std::cout << "successfully caught delete head when empty\n";
+        }
+        
+    } // list object dies ... list.~SLList() called
 
-    std::cout << "inserting head with 5 ...\n";
-    list.insert_head(5);
-    std::cout << list << '\n';
-    std::cout << list.head() << '\n';
+    // .....
     
     return 0;
 }
