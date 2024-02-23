8a6bbd660d90ca19c5d6937620732752139a1e94
Yihsiang Liow <yliow@ccis.edu>
2024-02-23 12:50:04 -0500

    no msg

--- 2024-02-23/main.cpp
+++ 2024-02-23/main.cpp
@@ -18,6 +18,21 @@ std::ostream & operator<<(std::ostream & cout, const SLNode & n)
     return cout;
 }
 
+class SLList
+{
+public:
+    SLList()
+        : phead_(NULL)
+    {}
+    void insert_head(int key)
+    {
+        SLNode * p = new SLNode(key);
+        p->next_ = phead_;
+        phead_ = p;
+    }
+    SLNode * phead_;
+};
+
 // 5 -> 3 -> 0 -> 2
 int main()
 {
@@ -29,22 +44,24 @@ int main()
     n3.next_ = &n0;
     n5.next_ = &n3;
     
-    std::cout << n5 << '\n';
-    std::cout << n3 << '\n';
-    std::cout << n0 << '\n';
-    std::cout << n2 << '\n';
+    // std::cout << n5 << '\n';
+    // std::cout << n3 << '\n';
+    // std::cout << n0 << '\n';
+    // std::cout << n2 << '\n';
 
     SLNode * phead = &n5;
 
     SLNode * p = phead;
     while (p != NULL)
     {
-        std::cout << (*p) << ' ';
+        std::cout << (*p) << '\n';
         p = p->next_;
     }
     
     // phead = &n5
     // SLList list // list.phead_
+    SLList list;
+    list.insert_head(1);
     
     return 0;
 }
