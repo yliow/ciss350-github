cd911f3ec65abacc17222a3643d67f87005e1f7a
Yihsiang Liow <yliow@ccis.edu>
2024-03-18 12:51:30 -0400

    no msg

--- 2024-03-15/main.cpp
+++ 2024-03-15/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <queue>
+#include <stack>
 
 class Node
 {
@@ -49,6 +51,53 @@ void preorder_print(Node * proot)
     }
 }
 
+void inorder_print(Node * proot)
+{
+    if (proot == NULL)
+    {
+        std::cout << "* ";
+    }
+    else
+    {
+        inorder_print(proot->left_);
+        std::cout << proot->key_ << ' ';
+        inorder_print(proot->right_);
+    }
+}
+
+void postorder_print(Node * proot)
+{
+    if (proot == NULL)
+    {
+        std::cout << "* ";
+    }
+    else
+    {
+        postorder_print(proot->left_);
+        postorder_print(proot->right_);
+        std::cout << proot->key_ << ' ';
+    }
+    // if u want to do postorder trav without recursion,
+    // copy th code from bf, change std::queue to std::stack
+}
+
+void bf_print(Node * proot)
+{
+    std::queue< Node * > q;
+    q.push(proot);
+    while (!q.empty())
+    {
+        Node * p = q.front();
+        q.pop();
+        if (p != NULL)
+        {
+            std::cout << p->key_ << ' ';
+            q.push(p->left_);
+            q.push(p->right_);
+        }
+    }
+}
+
 int main()
 {
     Node * p10 = new Node(10);
@@ -77,6 +126,9 @@ int main()
 
     std::cout << "preorder at p10 ... "; preorder_print(p10); std::cout << '\n';
     std::cout << "preorder at p8 ... "; preorder_print(p8); std::cout << '\n';
-        
+    std::cout << "inorder at p10 ... "; inorder_print(p10); std::cout << '\n';
+
+    std::cout << "bf_print at p10 ... "; bf_print(p10); std::cout << '\n';
+
     return 0;
 }
