c639185aecaa36b53cb061ea47d8c3756baee2fa
Yihsiang Liow <yliow@ccis.edu>
2024-11-22 12:37:57 -0500

    no msg

---
+++ 2024-11-20/knightstour/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+
+    std::vector< std::vector< bool > > board;
+    board[0].resize(
+    bool flag = bt_knights_tour(n, 
+    return 0;
+}
