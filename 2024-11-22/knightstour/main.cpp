7546aa7d735f597fcbe7e0c0b7b989e3010b8183
Yihsiang Liow <yliow@ccis.edu>
2024-11-22 13:44:50 -0500

    no msg

--- 2024-11-22/knightstour/main.cpp
+++ 2024-11-22/knightstour/main.cpp
@@ -31,7 +31,6 @@ std::ostream & operator<<(std::ostream & cout, const Board & board)
 std::ostream & operator<<(std::ostream & cout, const Solution & solution)
 {
     int n2 = int(solution.size());
-    std::cout << "n2:" << n2 << '\n';
     int n = sqrt(n2);
     std::vector< std::vector< int > > board(n, std::vector< int >(n, 0));
     for (int i = 0; i < n2; ++i)
