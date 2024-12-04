35cf08ae628833dc710ec373b77c7d9bcdf2f164
Yihsiang Liow <yliow@ccis.edu>
2024-12-04 12:28:32 -0500

    no msg

---
+++ 2024-12-04/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+class Node
+{
+public:
+    Node(int key, Node * left=NULL, Node * right=NULL)
+        : key_(key), left_(left), right_(right)
+    {}
+    int key_;
+    Node * left_;
+    Node * right_;
+};
+std::ostream & operator<<(std::ostream & cout, const Node & n)
+{
+    cout << "<Node " << &n << ' '
+         << n.key_ << ", "
+         << n.left_ << ", "
+         << n.right_ << '>';
+    return cout;
+}
+
+int main()
+{
+    /*
+            +---------+
+            |         |
+            v         |
+            0 -> 1 -> 2
+            |
+            v
+            3
+     */
+
+    Node * p0 = new Node(0);
+    Node * p1 = new Node(1);
+    Node * p2 = new Node(2);
+    Node * p3 = new Node(3);
+    p0->left_ = p3; p0->right_ = p1;
+    p1->left_ = p2;
+    p2->right_ = p0;
+    
+    
+    return 0;
+}
