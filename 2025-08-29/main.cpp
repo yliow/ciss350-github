f72f92f48330027841c716ba8fb0779709bbf55c
Yihsiang Liow <yliow@ccis.edu>
2025-08-29 12:53:04 -0400

    no msg

--- 2025-08-29/main.cpp
+++ 2025-08-29/main.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
 
-void print(int x[])
-{
-    return;
-}
+void println(int x[], int n);
+void print(int x[], int n);
+
 
 int main()
 {
     int x[] = {2, 3, 5, 7, 11, 13, 17, 19};
-    print(x);
+    print(x, 0, 5);
 
     return 0;
     
 }
+
+
+void print(int x[], int start, int end)
+{
+    std::cout << '{';
+    for (int i = start; i < end; ++i)
+    {
+        std::cout << x[i];
+        if (i < end - 1)
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << '}';
+    return;
+}
+
+
+void println(int x[], int start, int end)
+{
+    print(x, start, end);
+    std::cout << '\n';
+    return;
+}
+
