42b925ab80455ea19df910fa57d7cb9185541e90
Yihsiang Liow <yliow@ccis.edu>
2025-09-03 13:01:43 -0400

    no msg

--- 2025-09-03/Mat.cpp
+++ 2025-09-03/Mat.cpp
@@ -6,6 +6,11 @@ Mat::Mat(int nrows, int ncols)
     : nrows_(nrows), ncols_(ncols), p_(new double[nrows * ncols])
 {}
 
+Mat::~Mat()
+{
+    delete[] p_;
+}
+
 std::ostream & operator<<(std::ostream & cout, const Mat & m)
 {
     return cout;
