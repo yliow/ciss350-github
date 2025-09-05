8765522b8c9f4ebe6e9e0ec2266df6f791fa9ea5
Yihsiang Liow <yliow@ccis.edu>
2025-09-05 12:59:47 -0400

    no msg

--- 2025-09-05/main.cpp
+++ 2025-09-05/main.cpp
@@ -8,6 +8,8 @@ int main()
     // std::cout << m.ncols() << '\n';
     m(0, 0) = 42; // m.operator()(0, 0) = 42
     std::cout << m << '\n';
-    
+
+    Mat n(m); // Mat n = m;
+    std::cout << n << '\n';
     return 0;
 } // m.~Mat()
