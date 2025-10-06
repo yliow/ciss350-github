f692991fc9ec0cc0eb086a319b341552082aa158
Yihsiang Liow <yliow@ccis.edu>
2025-10-06 10:04:45 -0700

    no msg

--- 2025-10-06/main.cpp
+++ 2025-10-06/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
+class SLNode
+{
+public:
+    SLNode(int key, SLNode * next=NULL)
+        : key_(key), next_(next)
+    {}
+    int key_;
+    SLNode * next_;
+};
+
+std::ostream & operator<<(std::ostream & cout, const SLNode & n)
+{
+    cout << "<SLNode " << &n << ' '
+         << "key:" << n.key_ << ", "
+         << "next:" << n.next_ << '>';
+    return cout;
+}
+
 int main()
 {
+    SLNode n5(5);
+    std::cout << n5 << '\n';
     return 0;
 }
