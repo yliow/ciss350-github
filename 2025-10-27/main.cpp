4f86609e629a05aa47631df4e72d0f27032071e1
Yihsiang Liow <yliow@ccis.edu>
2025-10-29 09:16:58 -0700

    no msg

--- 2025-10-27/main.cpp
+++ 2025-10-27/main.cpp
@@ -8,7 +8,7 @@
 
       7      9           1
 
-                11
+                12
  */
 
 class Node
@@ -33,9 +33,42 @@ std::ostream & operator<<(std::ostream & cout, const Node & node)
     return cout;
 }
 
+void preorder_print(Node * proot)
+{
+    if (proot == NULL)
+    {
+        std::cout << "* ";
+    }
+    else
+    {
+        std::cout << proot->key_ << ' ';
+        preorder_print(proot->left_);
+        preorder_print(proot->right_);
+    }
+}
+
+
 int main()
 {
     Node * n11 = new Node(11);
+    Node * n3 = new Node(3); n3->parent_ = n11; n11->left_ = n3;
+    Node * n20 = new Node(20); n20->parent_ = n11; n11->right_ = n20;
+    Node * n7 = new Node(7); n7->parent_ = n3; n3->left_ = n7;
+    Node * n9 = new Node(9); n9->parent_ = n3; n3->right_ = n9;
+    Node * n1 = new Node(1); n1->parent_ = n20; n20->left_ = n1;
+    Node * n12 = new Node(12); n12->parent_ = n9; n9->right_ = n12;
+    
     std::cout << (*n11) << '\n';
+    std::cout << (*n3) << '\n';
+    std::cout << (*n20) << '\n';
+    std::cout << (*n7) << '\n';
+    std::cout << (*n9) << '\n';
+    std::cout << (*n1) << '\n';
+    std::cout << (*n12) << '\n';
+
+    Node * proot = n11;
+
+    preorder_print(proot); std::cout << '\n';
+    
     return 0;
 }
